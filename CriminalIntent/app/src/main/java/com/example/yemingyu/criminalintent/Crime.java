a42a7e94e76f27d01b09f51cd46eeaca97ab7dc8
夜禹 <mingyu.ymy@alipay.com>
2017-07-06 11:35:17 +0800

    add intent

--- CriminalIntent/app/src/main/java/com/example/yemingyu/criminalintent/Crime.java
+++ CriminalIntent/app/src/main/java/com/example/yemingyu/criminalintent/Crime.java
@@ -13,6 +13,15 @@ public class Crime {
     private String mTitle;
     private Date mDate;
     private boolean mSolved;
+    private String mSuspect;
+
+    public String getSuspect() {
+        return mSuspect;
+    }
+
+    public void setSuspect(String suspect) {
+        mSuspect = suspect;
+    }
 
     public Crime() {
         this(UUID.randomUUID());
